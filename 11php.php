cd4c7f91b81d6759622bf2fd4b6632389babbffc
KeiLazu <kennixlazuardi@gmail.com>
2017-10-30 10:35:59 +0700

    First Commit

---
+++ 11php.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <?php echo "Hello, World!"; ?><br>
+    <? echo "Apakah ini diperbolehkan?"; ?>
+    <script lang="php"> Masih diperbolehkan kah </script>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gfday1
 In Training Laravel Day 1
+
+On the first day of Training,
+
+i forgot what i wanna write lol
+
+---- nothing to show again -----
