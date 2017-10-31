ef921d26f00e1180b15f370412d0becb1270cffb
KeiLazu <kennixlazuardi@gmail.com>
2017-10-31 07:45:40 +0700

    finish

--- 14phpnonoverkill.php
+++ 14phpnonoverkill.php
@@ -1,8 +1,14 @@
 <?php
-    $keigVarA = 42;
-    $result = ($keigVarA < 20) ? 
-        ("$keigVarA is less than 20") : (($keigVarA > 30) ? 
-            ("$keigVarA is more Than 30") : "$keigVarA is in the middle in 20 and 30");
+    $keigVarA = 123;
+
+    if (is_numeric($keigVarA)) {
+        $result = ($keigVarA < 20) ? 
+            ("$keigVarA is less than 20") : (($keigVarA > 30) ?
+                ("$keigVarA is more Than 30") : "$keigVarA is in the middle in 20 and 30");   
+    } else {
+        $result = "Please put number in keigVarA";
+
+    }
 
     echo $result;
 
---
+++ createbygitconsole/createdagainbygitbash.php
@@ -0,0 +1,6 @@
+<?php
+echo "error\n\n";
+
+echo "direct call is forbidden";
+
+?>
